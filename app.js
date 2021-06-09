72f5a53da0f8a9a2f0e85206bd4c57de148e8729
MD Ripon Islam <islamripon876@gmail.com>
2021-06-09 23:51:48 +0600

    author will get email with examine and her result

--- app.js
+++ app.js
@@ -12,14 +12,46 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/",function(req,res){
-  // res.send("DFssfdgdgsdf gsdfg fd ");
-  // res.json({ username: 'Flavio' })
   res.sendFile(path.join(__dirname+'/index.html'));
 })
 
-app.post("/result",function(req,res){
-  console.log('')
-  // res.send("you got 13 out of 15")
+app.post("/result/:examine/:result",function(req,res){
+ 
+ var examine = req.params.examine;
+ var result  = req.params.result;
+
+  console.log(examine,result)
+res.json({ username: 'Flavio' });
+
+// node mailer
+
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL_ADDRESS,
+        pass: process.env.GMAIL_PASSWORD
+      }
+    });
+    // mdforhadhossain297@gmail.com
+    var mailOptions = {
+      from: process.env.GMAIL_ADDRESS,
+      to: "islam876ripon@gmail.com",
+      subject: "A new examine taken this test",
+      text: `${examine} has taken the test and got a result of ${result} out of 15`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      var title = ""
+      if (error) {
+        console.log(error);
+        res.render("404",{title: title,currenUser: req.user});
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.send("email successfully sent..");
+      }
+    });
+
+
 })
 
 
