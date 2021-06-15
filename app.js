8dd10ad29c3346c3ac0aed321c205dd3d9af95ec
MD Ripon Islam <islamripon876@gmail.com>
2021-06-15 22:19:34 +0600

    receiver email updated

--- app.js
+++ app.js
@@ -19,37 +19,49 @@ app.post("/result/:examine/:result",function(req,res){
  
  var examine = req.params.examine.replace(/_/g, ' ');
  var result  = req.params.result;
+ let mailList = "";
 
-  console.log(examine,result)
-res.json({ username: 'Flavio' });
+  // console.log(examine,result) //for debug
+
+
+if (process.env.TWO_RECEIVER === "yes") {
+   mailList = process.env.RECEIVER_EMAIL_1 + "," + process.env.RECEIVER_EMAIL_2;
+   console.log(mailList);
+}else {
+     mailList = process.env.RECEIVER_EMAIL_1;
+        console.log(mailList);
+}
 
 // node mailer
 
-    var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.GMAIL_ADDRESS,
-        pass: process.env.GMAIL_PASSWORD
-      }
-    });
-    // mdforhadhossain297@gmail.com
-    var mailOptions = {
-      from: process.env.GMAIL_ADDRESS,
-      to: "islam876ripon@gmail.com",
-      subject: "A new examine taken the test",
-      text: `${examine} has taken the test and got a result of ${result} out of 15`
-    };
+    // var transporter = nodemailer.createTransport({
+    //   service: 'gmail',
+    //   auth: {
+    //     user: process.env.GMAIL_ADDRESS,
+    //     pass: process.env.GMAIL_PASSWORD
+    //   }
+    // });
 
-    transporter.sendMail(mailOptions, function(error, info){
-      var title = ""
-      if (error) {
-        console.log(error);
-        res.render("404",{title: title,currenUser: req.user});
-      } else {
-        console.log('Email sent: ' + info.response);
-        res.send("email successfully sent..");
-      }
-    });
+
+
+
+    // var mailOptions = {
+    //   from: process.env.GMAIL_ADDRESS,
+    //   to: mailList,
+    //   subject: "A new examine taken the test",
+    //   text: `${examine} has taken the test and got a result of ${result} out of 15`
+    // };
+
+    // transporter.sendMail(mailOptions, function(error, info){
+    //   var title = ""
+    //   if (error) {
+    //     console.log(error);
+    //     res.render("404",{title: title,currenUser: req.user});
+    //   } else {
+    //     console.log('Email sent: ' + info.response);
+    //     res.send("email successfully sent..");
+    //   }
+    // });
 
 
 })
@@ -61,5 +73,9 @@ app.listen(port,function(){
 
 
 
-
+// setInterval(function(){
+//   var code = 200;
+//   console.log("")
+//   console.log("server respons with: " + code + " status code")
+// },20000)
 
